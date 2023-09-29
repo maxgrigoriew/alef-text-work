e0358af58e18d89d68529b8d2963d0a76879de00
Max <sss123123111@yandex.ru>
2023-09-29 15:30:03 +0300

    init project

---
+++ src/components/IsFooter.vue
@@ -0,0 +1,15 @@
+<template>
+	<footer class="footer">
+		<div class="container">
+			<div class="footer__inner">
+				<span class="footer__copy">all rights reserved</span>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<style scoped>
+.read-the-docs {
+	color: #888;
+}
+</style>
---
+++ src/components/IsHeader.vue
@@ -0,0 +1,23 @@
+<template>
+	<header class="header">
+		<div class="container">
+			<div class="header__inner">
+				<a href="#" class="logo">
+					<img src="./../assets/images/logo.png" alt="Логотип компании Alef" />
+				</a>
+				<nav class="nav">
+					<ul class="nav__list">
+						<li class="nav__item"><a href="#" class="nav__link">Форма</a></li>
+						<li class="nav__item"><a href="#" class="nav__link">Превью</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+</template>
+
+<style scoped>
+.read-the-docs {
+	color: #888;
+}
+</style>
